bedcec754fd1ba73745868cabbe3eb879cc87a63
vijayakumardpr <vijayakumardpr@gmail.com>
2023-02-07 22:38:15 +0530

    lesson2

--- App.js
+++ App.js
@@ -14,29 +14,58 @@
 import React from "react"
 import ReactDOM  from "react-dom/client"
 
-const el = React.createElement("h1",{
-    id:"title",
-   style:{
-    color:"red"
- }
-},"Welc ome to React")
+// const el = React.createElement("h1",{
+//     id:"title",
+//     key:"h1",
+//    style:{
+//     color:"red"
+//  }
+// },"Welc ome to React")
 
-const el2 = React.createElement("h1",{
-    id:"title",
-   style:{
-    color:"grey"
- }
-},"Welcome to React2")
-
-
-const container = React.createElement("div",{},[el,el2])
-
+// const el2 = React.createElement("h2",{
+//     id:"title",
+//     key:"h2",
+//    style:{
+//     color:"grey"
+//  }
+// },"Welcome to React2")
 
 
-console.log(el);
+// const container = React.createElement("div",{},[el,el2])
+
+// const header = React.createElement("h1",{},"Namaste React")
+
+// const list1 = React.createElement("li",{key:"1"},"Home")
+// const list2 = React.createElement("li",{key:"2"},"About Us")
+// const list3 = React.createElement("li",{key:"3"},"Contact Us")
+
+// const uList = React.createElement("ul",{},[list1,list2,list3])
+
+// const container = React.createElement("div",{},[header,uList])
+
+// const container = <h1>Welcome to React</h1>
+
+// console.log(container)
+
+const Header = ()=>(
+    <h1>Header Variable</h1>
+)
+
+const HeaderComponent = () =>{
+     let isTrue = false;
+   return (<div>
+            
+            <Header/>
+            <h1>Welcome to React !!!!</h1>
+            <h2>Hello World</h2>
+        </div>)
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(container)
+root.render(<HeaderComponent/>)
 
 
-   
+ 
+ 
